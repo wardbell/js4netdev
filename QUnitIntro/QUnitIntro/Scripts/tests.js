f460072f7f7b8baf95a4346747a0653d1e2a5f87
Ward Bell <wardbell@hotmail.com>
2012-11-03 12:18:15 -0700

    Add Tools & Breeze ppt; add QunitIntro
    Added the other intro-to-JavaScript-client materials that I presented at
    F2012 Dev Connections

---
+++ QUnitIntro/QUnitIntro/Scripts/tests.js
@@ -0,0 +1,83 @@
+﻿test("always passes", function() {
+    ok(true, "it passed");
+});
+
+
+
+
+
+test("always fails", function () {
+    ok(false, "it fails");
+});
+
+
+
+
+
+test("type coercion", function () {
+    // JavaScript coerces a string "1" to an integer
+    ok(1 == '1', "should coerce '1' to 1");
+});
+
+
+
+
+
+test("assert with equals", function () {
+    equal(1, 1, "1 should be 1");
+});
+
+
+
+test("assert with deep equals", 2, function () {
+    var sut = { foo: "bar" };
+    notEqual(sut, { foo: "bar" },
+        "should not be equal because not same object");
+
+    deepEqual(sut, { foo: "bar" },
+        "objects should have value equality at every level");
+});
+
+
+
+
+test("qunit exception handling", function () {
+
+    var CustomError = function () { };// custom error type
+    CustomError.message = "custom error message";
+
+    raises(function () {
+        throw new CustomError();
+    }, CustomError, "must throw CustomError");
+
+
+});
+
+
+
+
+
+/*** Async Testing ***/
+
+// None of our tests should take longer than 2 seconds.
+QUnit.config.testTimeout = 3000;
+
+// not a fan of QUnity asyncTest method. Use stop() explicitly
+test("async test", 1, function () {
+
+    var msg;
+    someComplicatedSetupThatCouldFail();
+
+    stop(); // going async
+    setTimeout(function () {
+        msg = "Giants won the 2012 World Series"
+        assertGotMsg()
+    }, 2000);
+
+    function assertGotMsg() {
+        ok(msg, "Got the message: " + msg);
+        start(); // resume test
+    }
+
+    function someComplicatedSetupThatCouldFail() { }
+});
